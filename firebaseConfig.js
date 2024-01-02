129609fe1e59e4993787e49788ba7d067f7978b6
Your Namecrepsleyfox <you@example.comalexiscrepsleyfox@gmail.com>
2024-01-02 11:06:08 +0000

    step 1 auth

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBPX-bc6jhMYH120V430Ffj8UV7s9e6STA",
@@ -10,3 +12,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
