d2cfaf602152a8b74fe2ca4d9e60bd627f51e418
nicoMaz <139277771+nicomaz@users.noreply.github.com>
2024-01-02 09:24:54 +0000

    initial setup

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBPX-bc6jhMYH120V430Ffj8UV7s9e6STA",
+  authDomain: "side-quest-8ecea.firebaseapp.com",
+  projectId: "side-quest-8ecea",
+  storageBucket: "side-quest-8ecea.appspot.com",
+  messagingSenderId: "630136307356",
+  appId: "1:630136307356:web:fa60ec14570d9af029f759",
+};
+
+const app = initializeApp(firebaseConfig);
