a4fe7c472f37c8c65c4a2d1a994ba2511114d996
Fabelixio <felixsewellknight@gmail.com>
2024-01-02 16:51:21 +0000

    add user location to map view

--- App.js
+++ App.js
@@ -1,22 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Map from './MapComponent';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Map from "./MapComponent";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Map />
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.map}>
+     <Map />
       <StatusBar style="auto" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
 });
--- MapComponent.js
+++ MapComponent.js
@@ -1,51 +1,68 @@
-import MapView from "react-native-maps";
-import { StyleSheet } from "react-native";
-import { Marker } from "react-native-maps";
-import * as Location from 'expo-location'
-import React, { useState, useEffect } from 'react'
-
-const styles = StyleSheet.create({
-    map: {
-        height: '100%',
-        width: '100%',
-    }
-  });
-
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
+import MapViewDirections from "react-native-maps-directions";
 const Map = () => {
-    const [location, setLocation] = useState(null)
-
-    useEffect(() => {
-        Location.requestForegroundPermissionsAsync()
-        .then(({ status }) => {
-            if(status !== 'granted') {
-                return Promise.reject()
-            }
-        })
-        return Location.getCurrentPositionAsync({})
-        .then((location) => {
-            setLocation(location)
-        })
-    }, [])
-    const userLocation = {latitude: location.coords.latitude,
-    longitude: location.coords.longitude}
-   return (
-    <MapView style={styles.map}
-  initialRegion={{
-    latitude: 51.5072,
-    longitude: 0.1276,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
-  }}>
-        <Marker coordinate={{latitude: 51.5104,
-        longitude: -0.0971}}
-        />
-        <MapViewDirections 
-        origin={userLocation}
-        destination={{latitude: 51.5104,
-            longitude: -0.0871}}
-        apikey='AIzaSyDfMw0j4oBjMtX2Ja5MMCAfYmdW1SAji0A'/>
-    </MapView>
-   )
-}
-
-export default Map
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  useEffect(() => {
+    Location.requestForegroundPermissionsAsync()
+      .then(({ status }) => {
+        if (status !== "granted") {
+          setErrorMsg("Permission to access location was denied");
+          return Promise.reject("Permission denied");
+        }
+        return Location.getCurrentPositionAsync({});
+      })
+      .then((userLocation) => {
+        setLocation(userLocation);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+  return (
+    <View style={{ flex: 1 }}>
+      {location ? (
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+            title="Your Location"
+            description="You are here!"
+          />
+          <MapViewDirections
+            origin={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+            destination={{ latitude: 51.4808, longitude: -0.1227 }}
+            apikey="AIzaSyDfMw0j4oBjMtX2Ja5MMCAfYmdW1SAji0A"
+            mode="WALKING"
+          />
+        </MapView>
+      ) : (
+        <Text>Loading location...</Text>
+      )}
+      {errorMsg && <Text>{errorMsg}</Text>}
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  map: {
+    height: "100%",
+    width: '100%'
+  },
+});
+export default Map;
