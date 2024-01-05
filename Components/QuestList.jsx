de82570eb6c108b0c74f9d77afc807e14603913b
Inderpreet Sandhu <inderpreetsingh@hotmail.co.uk>
2024-01-05 15:30:22 +0000

    use firebase data for quest components

--- Components/QuestList.jsx
+++ Components/QuestList.jsx
@@ -1,78 +1,32 @@
 import { View, Text, FlatList, StyleSheet } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import QuestCard from "./QuestCard";
-
-//dummy data, TODO - replace w firebase data
-//landmarks for quests?
-const quests = [
-  {
-    questId: 1,
-    title: "Great Fire Quest",
-  },
-  {
-    questId: 2,
-    title: "Tower Bridge Quest",
-  },
-  {
-    questId: 3,
-    title: "Covent Garden Quest",
-  },
-];
-
-const questions = [
-  {
-    questId: 1,
-    questionId: 1,
-    text: "What year did the Great Fire of London occur?",
-    type: "multiple choice",
-    options: ["1666", "1776", "1692", "1888"],
-    correctAnswer: "1666",
-  },
-  {
-    questId: 1,
-    questionId: 2,
-    text: "How many houses burnt down in the Great Fire of London?",
-    type: "text input",
-    correctAnswer: "13200",
-  },
-  {
-    questId: 2,
-    questionId: 3,
-    text: "When was Tower Bridge officially opened?",
-    type: "multiple choice",
-    options: ["1886", "1894", "1923", "1950"],
-    correctAnswer: "1894",
-  },
-  {
-    questId: 2,
-    questionId: 4,
-    text: "How many towers does Tower Bridge have?",
-    type: "text input",
-    correctAnswer: "2",
-  },
-  {
-    questId: 3,
-    questionId: 5,
-    text: "Which famous market is located in Covent Garden?",
-    type: "multiple choice",
-    options: [
-      "Portobello Road Market",
-      "Camden Market",
-      "Covent Garden Market",
-      "Borough Market",
-    ],
-    correctAnswer: "Covent Garden Market",
-  },
-  {
-    questId: 3,
-    questionId: 6,
-    text: "Name one of the theaters in Covent Garden.",
-    type: "text input",
-    correctAnswer: "Royal Opera House",
-  },
-];
+import { collection, getDocs } from "firebase/firestore";
+import { db } from '../firebaseConfig';
 
 const QuestList = () => {
+  const [quests, setQuests] = useState([]);
+  const [questions, setQuestions] = useState([]);
+
+  const getFirestoreData = async () => {
+    const questsSnapshot = await getDocs(collection(db, 'quests'));
+    const questionsSnapshot = await getDocs(collection(db, 'questions'));
+    const allQuests = [];
+    const allQuestions = [];
+    questsSnapshot.forEach((doc) => {
+      allQuests.push(doc.data());
+    });
+    questionsSnapshot.forEach((doc) => {
+      allQuestions.push(doc.data());
+    });
+    setQuests(allQuests);
+    setQuestions(allQuestions);
+  }
+  
+  useEffect(() => {
+    getFirestoreData()
+  }, [])
+
   return (
     <View style={styles.container}>
       <View style={styles.questContainer}>
