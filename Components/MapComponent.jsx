ccc2769ef90a069cedae3b35f8631cbc3646d6f3
nicoMaz <139277771+nicomaz@users.noreply.github.com>
2024-01-06 16:31:29 +0000

    remove console log

--- Components/MapComponent.jsx
+++ Components/MapComponent.jsx
@@ -16,35 +16,31 @@ import {
   where,
 } from "firebase/firestore";
 
-
-
 const Map = () => {
   const [currentLocation, setCurrentLocation] = useState(null);
-  const [questLocations, setQuestLocations] = useState([])
-  const [questDestination, setQuestDestination] = useState({"latitude": 51.5138, "longitude": -0.0984});
+  const [questLocations, setQuestLocations] = useState([]);
+  const [questDestination, setQuestDestination] = useState({
+    latitude: 51.5138,
+    longitude: -0.0984,
+  });
   const [selectedMarker, setSelectedMarker] = useState(null);
   const [currentQuest, setCurrentQuest] = useState(null);
   const [render, setRender] = useState(null);
 
-
   const getFirestoreData = async () => {
-    const questsSnapshot = await getDocs(collection(db, 'quests'));
-  
+    const questsSnapshot = await getDocs(collection(db, "quests"));
+
     const allQuests = [];
     questsSnapshot.forEach((doc) => {
       allQuests.push(doc.data());
-     
     });
-    console.log(allQuests[0])
     setQuestLocations(allQuests);
-  }
+  };
 
+  const auth = getAuth(app);
+  const user = auth.currentUser;
 
-
-  const auth = getAuth(app)
-  const user = auth.currentUser
-
-  async function getUser () {
+  async function getUser() {
     const docRef = doc(db, "users", user.phoneNumber);
     const docSnap = await getDoc(docRef);
     setCurrentQuest(docSnap.data().currentQuest);
@@ -60,8 +56,6 @@ const Map = () => {
     });
   }
 
-
-
   useEffect(() => {
     (async () => {
       try {
@@ -70,8 +64,8 @@ const Map = () => {
           console.log("Permission to access location was denied");
           return;
         }
-  
-        getFirestoreData()
+
+        getFirestoreData();
         await getUser();
 
         const locationSubscription = await Location.watchPositionAsync(
@@ -175,6 +169,5 @@ const styles = StyleSheet.create({
   },
 });
 export default Map;
-
 
 //51.511087475628955, -0.08601434783572807
