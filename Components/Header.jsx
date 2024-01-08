58efbd69c94c85f7bb3e6ed3d8a882d0ff0eda30
nicoMaz <139277771+nicomaz@users.noreply.github.com>
2024-01-08 07:22:27 +0000

    normalise drawer styling

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,42 +1,29 @@
-import { Text, StyleSheet, SafeAreaView } from "react-native";
+import { Text, StyleSheet, SafeAreaView, View } from "react-native";
 import React from "react";
 import { getAuth } from "firebase/auth";
 import { app } from "../firebaseConfig";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
 
 const Header = (props) => {
   const auth = getAuth(app);
   const user = auth.currentUser;
+  const navigation = useNavigation();
 
   return (
-    <SafeAreaView style={styles.header}>
-      {/* <Text style={styles.pageName}>{props.name}</Text> */}
-      <Text style={styles.headerText}>SideQuest</Text>
-      <Text style={styles.user}>Hello {user.displayName}</Text>
+    <SafeAreaView>
+      <View className="flex flex-row justify-between w-screen pr-24">
+        <Text className="font-bold text-white text-xl tracking-wide">
+          SideQuest
+        </Text>
+        {/* <TouchableOpacity onPress={() => navigation.navigate("Profile")}>
+          <Text className="self-center text-right justify-end text-white text-lg font-bold">
+            {user.displayName}
+          </Text>
+        </TouchableOpacity> */}
+      </View>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  header: {
-    width: "100%",
-    height: "100%",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  headerText: {
-    fontWeight: "bold",
-    fontSize: 26,
-    color: "white",
-    letterSpacing: 2,
-  },
-  pageName: {
-    justifyContent: "flex-start",
-  },
-  user: {
-    justifyContent: "flex-start",
-    color: "#415177",
-  },
-});
 
 export default Header;
