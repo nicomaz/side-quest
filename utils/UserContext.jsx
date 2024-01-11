4da94155ae1e690c4297fedd6b2a3c19616b201f
nicoMaz <139277771+nicomaz@users.noreply.github.com>
2024-01-11 09:54:52 +0000

    userContext with snapshot

--- utils/UserContext.jsx
+++ utils/UserContext.jsx
@@ -1,24 +1,30 @@
-import { createContext, useEffect, useState, } from "react"
-import { getUser } from "./api"
+import { createContext, useEffect, useState } from "react";
+import { doc, onSnapshot } from "firebase/firestore";
+import { app } from "firebase-admin";
+import { getAuth } from "firebase/auth";
+import { db } from "../firebaseConfig";
 
-export const UserContext = createContext()
+export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-    const [user, setUser] = useState({})
-    const [completeQuests, setCompleteQuests] = useState([])
-    useEffect(() => {
-        const fetchUser = async () => {
-           await getUser().then((res) => {
-            setUser(res)
-           })
-        }
-        fetchUser()
-        console.log(user)
-    }, [])
-    return(
-        <UserContext.Provider value={{ user, setUser }}>
-            {children}
-        </UserContext.Provider>
-    )
-}
+  const [userData, setUserData] = useState();
+
+  const auth = getAuth(app);
+  const user = auth.currentUser;
+
+  useEffect(() => {
+    const fetchUser = onSnapshot(doc(db, "users", user.phoneNumber), (doc) => {
+      setUserData(doc.data());
+    });
+    fetchUser();
+  }, []);
+
+  console.log(userData);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
 // Need to keep track of state here? to update when a quest completes, triggering a rerender?
