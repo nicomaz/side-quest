0daff8398366457ad7e08896936b690bd0958f96
nicoMaz <139277771+nicomaz@users.noreply.github.com>
2024-01-11 09:07:04 +0000

    edit homescreen header height

--- NavScreens.js
+++ NavScreens.js
@@ -24,7 +24,7 @@ function DrawerNavigation() {
             drawerActiveBackgroundColor: {
               backgroundColor: "#344c76",
             },
-            headerStyle: { backgroundColor: "#344c76" },
+            headerStyle: { backgroundColor: "#344c76", height: 110 },
             headerTitle: "",
             headerTintColor: "white",
             headerRight: () => <Header />,
